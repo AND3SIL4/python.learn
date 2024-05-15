3c721761b373a45737b624466b17f909abb394d8
AND3SIL4 <afsilva6236@misena.edu.co>
2024-05-14 19:00:49 -0500

    feat(app): new exercice added

--- poo/poly/basic.py
+++ poo/poly/basic.py
@@ -1,4 +1,10 @@
 """
   ¿Que es el polimorfismo?:
   El polimorfismo es un patrón de diseño que se utiliza para definir una clase que hereda de otras clases.
+  clases respondan a un mismo mensaje de diferentes maneras
 """
+
+def otro():
+  """
+  """
+  pass
