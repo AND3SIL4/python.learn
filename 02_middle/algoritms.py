073f7abcb5579d41ac155286a2b3fd107f3b2b1a
AND3SIL4 <afsilva6236@misena.edu.co>
2025-05-20 21:32:46 -0500

    fix(app): structure to learn the python path

---
+++ 02_middle/algoritms.py
@@ -0,0 +1,7 @@
+"""
+NOTES:
+    The algorithms are a way to solve a problem.
+        - We can calculate the complexity of an algorithm
+            - Linear complexity: O(n)
+            - Constant complexity: 0(1)
+"""
