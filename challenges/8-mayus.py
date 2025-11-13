fd1a658436715d926ac1f06dfd9d364c3ad9724f
AND3SIL4 <devaul.fs@gmail.com>
2025-11-13 08:34:22 -0500

    update: file 8 parse the base tests

--- challenges/8-mayus.py
+++ challenges/8-mayus.py
@@ -15,6 +15,7 @@ import pytest
         ("andres felipe silva", "Andres Felipe Silva"),
         ("Andres Silva", "Andres Silva"),
         ("andres", "Andres"),
+        ("anDRes", "Andres"),
     ],
 )
 def test_capitalize_string(string: str, expected: str) -> None:
@@ -31,10 +32,35 @@ def test_capitalize_string(string: str, expected: str) -> None:
         ("", ValueError, "The input string is empty"),
     ],
 )
-def wrong_capitalize_string(string: str, error_type: Exception, expected: str) -> None:
+def test_wrong_capitalize_string(
+    string: str, error_type: Exception, expected: str
+) -> None:
     with pytest.raises(error_type, match=expected):
         capitalize_string(string=string)
 
 
 # Make de main logic for passing the tests
-def capitalize_string(string) -> str: ...
+def capitalize_string(string) -> str:
+    # 1. Validate if the input type is correct
+    if not isinstance(string, str):
+        raise TypeError("The input type is not valid")
+
+    # 2. Validate if the string is empty
+    if not string:
+        raise ValueError("The input string is empty")
+
+    # 3. Make the logic for parsing to capitalize the string
+    # 3.1 Split the string to complete words as needed
+    words = string.split(" ")
+
+    # 3.2 Iterate over all words and validate the firts character of the words
+    for i, word in enumerate[str](words):
+        # Take the first letter of the word
+        if not word[0].isupper():
+            words[i] = word[0].upper() + word[1:]
+
+    return " ".join(words)
+
+
+print(capitalize_string("andres felipe silva"))
+print(capitalize_string("Andres"))
