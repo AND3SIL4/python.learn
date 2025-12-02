f5192dcd0f1fc87e7e4ebaf7bb998b738fa62b1e
AND3SIL4 <devaul.fs@gmail.com>
2025-12-02 10:33:37 -0500

    update: finished the 13 file problem

--- challenges/13-sort_list.py
+++ challenges/13-sort_list.py
@@ -15,7 +15,7 @@ from typing import Literal
     "list_to_be_sorted, order, expected", [([2, 4, 6, 8, 9], "Desc", [9, 8, 6, 4, 2])]
 )
 def test_sort_list(
-    list_to_be_sorted: list[int], order: Literal["Asc, Desc"], expected
+    list_to_be_sorted: list[int], order: Literal["Asc", "Desc"], expected
 ) -> None:
     assert sort_list(list_to_be_sorted=list_to_be_sorted, order=order) == expected
 
@@ -29,12 +29,24 @@ def test_sort_list(
             "Desc",
             TypeError,
             "The input type is not valid, please try again",
-        )
+        ),
+        (
+            ["2", 4.3, "6", 8, 9.3],
+            "Desc",
+            TypeError,
+            "The input type is not valid, please try again",
+        ),
+        (
+            [2, 4, 8, 9],
+            "Another",
+            ValueError,
+            "The order argument is not valid",
+        ),
     ],
 )
 def test_negative_sort_list(
     list_to_be_sorted: list[int],
-    order: Literal["Asc, Desc"],
+    order: Literal["Asc", "Desc"],
     error_type: Exception,
     expected: str,
 ) -> None:
@@ -42,4 +54,49 @@ def test_negative_sort_list(
         sort_list(list_to_be_sorted=list_to_be_sorted, order=order)
 
 
-def sort_list(list_to_be_sorted, order: Literal["Asc, Desc"]) -> list[int]: ...
+def sort_list(list_to_be_sorted, order: Literal["Asc", "Desc"]) -> list[int]:
+    # Validate if the input are valid
+    # Get the data types
+    sub_types = set[str](isinstance(i, int) for i in list_to_be_sorted)
+    if not all(sub_types):
+        raise TypeError("The input type is not valid, please try again")
+
+    n = len(list_to_be_sorted)
+
+    match order:
+        case "Asc":
+            swapped = False
+            for j in range(n):
+                for i in range(0, n - j - 1):
+                    if list_to_be_sorted[i] > list_to_be_sorted[i + 1]:
+                        list_to_be_sorted[i + 1], list_to_be_sorted[i] = (
+                            list_to_be_sorted[i],
+                            list_to_be_sorted[i + 1],
+                        )
+                        swapped = True
+
+                if not swapped:
+                    break
+            return list_to_be_sorted
+
+        case "Desc":
+            swapped = False
+            for j in range(n):
+                for i in range(0, n - j - 1):
+                    if list_to_be_sorted[i + 1] > list_to_be_sorted[i]:
+                        list_to_be_sorted[i + 1], list_to_be_sorted[i] = (
+                            list_to_be_sorted[i],
+                            list_to_be_sorted[i + 1],
+                        )
+                        swapped = True
+
+                if not swapped:
+                    break
+            return list_to_be_sorted
+        case _:
+            raise ValueError("The order argument is not valid")
+
+
+print(sort_list(list_to_be_sorted=[2, 4, 6, 8, 9], order="Desc"))
+# print(sort_list(list_to_be_sorted=[140, 56, 2, 1], order="Asc"))
+# print(sort_list(list_to_be_sorted=[2, 4.3, 6, 8, 9.3], order="Desc"))
