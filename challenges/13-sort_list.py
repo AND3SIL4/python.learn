31a3b33391a0aaebb0014c0358e81ea03c3b6001
AND3SIL4 <devaul.fs@gmail.com>
2025-12-02 07:50:01 -0500

    update apply the base tdd structure for sort arrays

--- challenges/13-sort_list.py
+++ challenges/13-sort_list.py
@@ -4,3 +4,42 @@
 #   o de mayor a menor.
 # - No se pueden utilizar funciones propias del lenguaje que lo resuelvan
 #   automáticamente.
+
+# Utilizar TDD (test driven development) to solve the problem
+import pytest
+from typing import Literal
+
+
+# Make the assertive tests
+@pytest.mark.parametrize(
+    "list_to_be_sorted, order, expected", [([2, 4, 6, 8, 9], "Desc", [9, 8, 6, 4, 2])]
+)
+def test_sort_list(
+    list_to_be_sorted: list[int], order: Literal["Asc, Desc"], expected
+) -> None:
+    assert sort_list(list_to_be_sorted=list_to_be_sorted, order=order) == expected
+
+
+# Make the negative tests
+@pytest.mark.parametrize(
+    "list_to_be_sorted, order, error_type, expected",
+    [
+        (
+            [2, 4.3, 6, 8, 9.3],
+            "Desc",
+            TypeError,
+            "The input type is not valid, please try again",
+        )
+    ],
+)
+def test_negative_sort_list(
+    list_to_be_sorted: list[int],
+    order: Literal["Asc, Desc"],
+    error_type: Exception,
+    expected: str,
+) -> None:
+    with pytest.raises(error_type, match=expected):
+        sort_list(list_to_be_sorted=list_to_be_sorted, order=order)
+
+
+def sort_list(list_to_be_sorted, order: Literal["Asc, Desc"]) -> list[int]: ...
