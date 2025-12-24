18635b77b35bfd1ba3d0a9462ac09bb37456b3bc
AND3SIL4 <devaul.fs@gmail.com>
2025-12-24 07:32:34 -0500

    build: create a simple file to test the string features

---
+++ roadmapsh/strings.py
@@ -0,0 +1,22 @@
+"""
+No 12 characters
+No spaces
+No digits
+"""
+
+name = input("Type the name: ")
+print(name[7:4:-1])
+
+# if len(name) > 12:
+#     raise ValueError("The input exceed the lenght limit")
+
+# if not name.find(" ") == -1:
+#     raise ValueError("The input shall no contain spaces")
+
+# if not name.isalpha():
+#     raise ValueError("The input shoud not containt digits")
+
+
+"""
+
+"""
