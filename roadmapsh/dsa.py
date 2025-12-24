137ba7ae83b12b218da85fd4303ee11eb626e834
AND3SIL4 <devaul.fs@gmail.com>
2025-12-24 07:32:06 -0500

    new: dsa and algorithms file

---
+++ roadmapsh/dsa.py
@@ -0,0 +1,18 @@
+"""
+What are data structures?
+    - Just a format to store data in the computer, in order to keep the data organized and efficiently
+
+What exactly is an algorithm?
+    - A series of step-by-step to do for solving a computation problem
+    Algorithm, depends on the model of computation:
+        - Desing time
+        - Domain language
+        - Analyze
+        - Input and output
+    Program: 
+        - Implementation
+        - Programmer
+        - Testing
+Important facts
+    - The linked list is more effient to resize than the array list
+"""
