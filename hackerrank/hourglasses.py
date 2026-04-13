cff674345fa5b4a9c8bd6fbe1396e2d85106113f
AND3SIL4 <devaul.fs@gmail.com>
2026-04-13 16:18:57 -0500

    fix: increase the perform of the hourglass challenge hackerrank

--- hackerrank/hourglasses.py
+++ hackerrank/hourglasses.py
@@ -1,42 +1,35 @@
-def get_submatrix(matrix, row_init, row_end, col_init, col_end) -> list[list]:
-    # Base on its location return a submatrix required
-    return [row[col_init:col_end] for row in matrix[row_init:row_end]]
-
-
-def sum_submatrix(submatrix) -> int:
-    # All the matrix are 3x3
-    # In the middle row, we only have to sum the middle value
-    # Whatever the value would be
-    return sum(submatrix[0]) + submatrix[1][1] + sum(submatrix[2])
-
-
 def hourglassSum(arr) -> int:
-    # Define the submatrix length
-    lengt_submatrix = 3
-    # Create a vector to store the values of each submatrix
-    results = []
-    for row_index, row in enumerate(arr):
-        for col_idx, col in enumerate(row):
-            submatrix = get_submatrix(
-                arr,
-                row_index,
-                row_index + lengt_submatrix,
-                col_idx,
-                col_idx + lengt_submatrix,
+    # Define size of the matrix
+    # Matrix cols length
+    # Martix rows length
+    rows = len(arr)
+    cols = len(arr[0])
+    # Create a variable to store the max value
+    max_value = 0
+
+    # Iterate over the length of the rows
+    for i in range(rows - 2):
+        # Iterate over the length of the cols
+        for j in range(cols - 2):
+            # Calculate the sum of the values simulating a submatrix
+            total = (
+                ((arr[i][j]) + (arr[i][j + 1]) + arr[i][j + 2])
+                + (arr[i + 1][j + 1])
+                + ((arr[i + 2][j]) + (arr[i + 2][j + 1]) + (arr[i + 2][j + 2]))
             )
-            if len(submatrix) == 3 and len(submatrix[0]) == 3:
-                total = sum_submatrix(submatrix)
-                results.append(total)
-    return max(results)
+            # Replace the current value with the total calculated
+            if total > max_value:
+                max_value = total
+    return max_value  # Return result
 
 
 if __name__ == "__main__":
     matrix = [
-        [1, 1, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [1, 1, 1, 0, 0, 0],
-        [0, 0, 2, 4, 4, 0],
-        [0, 0, 0, 2, 0, 0],
-        [0, 0, 1, 2, 4, 0],
+        [0, -4, -6, 0, -7, -6],
+        [-1, -2, -6, -8, -3, -1],
+        [-8, -4, -2, -8, -8, -6],
+        [-3, -1, -2, -5, -7, -4],
+        [-3, -5, -3, -6, -6, -6],
+        [-3, -6, 0, -8, -6, -7],
     ]
     print(hourglassSum(arr=matrix))
