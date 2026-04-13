300c3d442906df7caa3637c56dea0d85d386bbd7
AND3SIL4 <devaul.fs@gmail.com>
2026-04-13 15:56:09 -0500

    feat: create a new version of the problem solving

--- hackerrank/hourglasses.py
+++ hackerrank/hourglasses.py
@@ -1,20 +1,33 @@
-def hourglassSum(arr):
-    # Iterate over the array and gets the rows
-    # Get the length of the row and initialize the start index
-    # length = len(arr[0]) - 1
-    # hourglasses_matrix = []
-    # for row in arr:
-    #     hourglass = []
-    #     for i in range(length):
-    #         for _ in range(3):
-    #             hourglass.append(row[i : i + 3])
-    #             print(row[i : i + 3])
+def get_submatrix(matrix, row_init, row_end, col_init, col_end) -> list[list]:
+    # Base on its location return a submatrix required
+    return [row[col_init:col_end] for row in matrix[row_init:row_end]]
 
-    #         print()
 
-    # print(len(hourglasses_matrix))
-    tres_filas = arr[0:3:]
-    print(tres_filas[0][1:4])
+def sum_submatrix(submatrix) -> int:
+    # All the matrix are 3x3
+    # In the middle row, we only have to sum the middle value
+    # Whatever the value would be
+    return sum(submatrix[0]) + submatrix[1][1] + sum(submatrix[2])
+
+
+def hourglassSum(arr) -> int:
+    # Define the submatrix length
+    lengt_submatrix = 3
+    # Create a vector to store the values of each submatrix
+    results = []
+    for row_index, row in enumerate(arr):
+        for col_idx, col in enumerate(row):
+            submatrix = get_submatrix(
+                arr,
+                row_index,
+                row_index + lengt_submatrix,
+                col_idx,
+                col_idx + lengt_submatrix,
+            )
+            if len(submatrix) == 3 and len(submatrix[0]) == 3:
+                total = sum_submatrix(submatrix)
+                results.append(total)
+    return max(results)
 
 
 if __name__ == "__main__":
